d840b3e9a78c779493a07c3b4f79a8ffb7c135d0
MichalLopatka <michalblopatka@gmail.com>
2022-11-19 19:51:42 +0100

    initial

---
+++ main.py
@@ -0,0 +1,63 @@
+import numpy as np
+from deslib.des.knora_e import KNORAE
+from deslib.des.knora_u import KNORAU
+import glob
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.base import clone
+from sklearn.ensemble import RandomForestClassifier
+from strlearn.metrics import recall, precision, specificity, f1_score, geometric_mean_score_1
+
+
+
+datasets = glob.glob("datasets/*.dat")
+
+metrics = {
+    "recall": recall,
+    'precision': precision,
+    'specificity': specificity,
+    'f1': f1_score,
+    'g-mean': geometric_mean_score_1,
+}
+
+print(datasets)
+cls_dict = {"positive": 1, "negative": 0}
+n_datasets = len(datasets)
+n_splits = 5
+n_repeats = 2
+rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=1234)
+
+scores = np.zeros((2, n_datasets, n_splits * n_repeats, len(metrics)))
+
+for data_id, dataset in enumerate(datasets):
+    dataset = np.genfromtxt(dataset, delimiter=",", comments="@",  dtype='unicode')
+    X = dataset[:, :-1].astype(float)
+    y = dataset[:, -1]
+    y = np.char.strip(y)
+    y = [cls_dict[el] for el in y]
+    y = np.array(y)
+    # print(y)
+    # print(X)
+    pool_classifiers = RandomForestClassifier(n_estimators=15)
+    pool_classifiers.fit(X, y)
+    clfs = {
+        'KNORAU': KNORAU(pool_classifiers),
+        'KNORAE': KNORAE(pool_classifiers)
+    }
+    for fold_id, (train, test) in enumerate(rskf.split(X, y)):
+        for clf_id, clf in enumerate(clfs):
+
+            clfs[clf].fit(X[train], y[train])
+            y_pred = clfs[clf].predict(X[test])
+
+            for metric_id, metric in enumerate(metrics):
+                # print(metric, metrics[metric](y[test], y_pred))
+                scores[clf_id, data_id, fold_id, metric_id] = metrics[metric](
+                    y[test], y_pred)
+
+np.save('results', scores)
+
+
+scores = np.load('results.npy')
+mean_scores = np.mean(scores, axis=2).T
+print("\nMean scores:\n", mean_scores)
+
