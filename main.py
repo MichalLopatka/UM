f37b80d133aed8de1d7bedfa42200856a7c6825e
MichalLopatka <michalblopatka@gmail.com>
2022-12-13 14:52:16 +0100

    added radar plot

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from sklearn.base import clone
 from sklearn.ensemble import RandomForestClassifier
 from strlearn.metrics import recall, precision, specificity, f1_score, geometric_mean_score_1
 from knoranew import KNORA_U
-
+import matplotlib.pyplot as plt
+from math import pi
 datasets = glob.glob("datasets/*.dat")
 
 metrics = {
@@ -61,6 +62,45 @@ np.save('results', scores)
 
 
 scores = np.load('results.npy')
-mean_scores = np.mean(scores, axis=2).T
-print("\nMean scores:\n", mean_scores)
+scores = np.mean(scores, axis=2).T
+print("\nMean scores:\n", scores)
+scores[np.isnan(scores)] = 0
+scores = np.mean(scores, axis=1)
 
+
+# metryki i metody
+metrics=['Precision', "Recall",'Specificity', 'F1', 'G-mean']
+methods=["KNORAU", 'KNORAE', 'KNORAU-IM']
+N = scores.shape[0]
+
+# kat dla kazdej z osi
+angles = [n / float(N) * 2 * pi for n in range(N)]
+angles += angles[:1]
+
+# spider plot
+ax = plt.subplot(111, polar=True)
+
+# pierwsza os na gorze
+ax.set_theta_offset(pi / 2)
+ax.set_theta_direction(-1)
+
+# po jednej osi na metryke
+plt.xticks(angles[:-1], metrics)
+
+# os y
+ax.set_rlabel_position(0)
+plt.yticks([0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],
+["0.0","0.1","0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9","1.0"],
+color="grey", size=7)
+plt.ylim(0,1)
+
+# Dodajemy wlasciwe ploty dla kazdej z metod
+for method_id, method in enumerate(methods):
+    values=scores[:, method_id].tolist()
+    values += values[:1]
+    ax.plot(angles, values, linewidth=1, linestyle='solid', label=method)
+
+# Dodajemy legende
+plt.legend(bbox_to_anchor=(1.15, -0.05), ncol=5)
+# Zapisujemy wykres
+plt.savefig("radar", dpi=200)
